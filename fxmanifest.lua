14b094439191bf4bcfe2ec22e38439f4c0e2d386
Dev Fivem <stevenmuller29@sfr.fr>
2025-01-12 01:41:16 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+loadscreen 'index.html'
+
+files {
+    'index.html',
+    'background.png', -- Remplacez par le nom de votre image de fond
+    --'loading-bar.png', -- Remplacez par le nom de votre barre de chargement
+    'music.mp3' -- Remplacez par le nom de votre fichier audio
+}
